0d0e0da3bce3c6f6cd847e4ced1b918ff32e7a80
Yash Rai <yash2154rai@gmail.com>
2026-04-05 23:23:46 +0530

    update cursor 2.6 to 3.0

--- components/ReleaseBanner.tsx
+++ components/ReleaseBanner.tsx
@@ -4,7 +4,7 @@ import { VscMegaphone } from "react-icons/vsc";
 // UPDATE THIS CONFIGURATION TO ANNOUNCE NEW RELEASES
 export const RELEASE_CONFIG = {
   isVisible: true,
-  text: "🚀 Android Studio & PyCharm support is now live in Scriptly! Install them today.",
+  text: "🚀 Cursor 3.0 support is now live in Scriptly! Install it today.",
   link: "/install",
 };
 
