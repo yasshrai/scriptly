f6bf75030016f42eea66705508e99df8fde8aed3
Yash Rai <yash2154rai@gmail.com>
2026-04-06 15:09:23 +0530

    antigravity 1.21.6 -> 1.21.9

--- public/scripts/installantigravity.sh
+++ public/scripts/installantigravity.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-URL="https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/1.21.6-5723021441368064/linux-x64/Antigravity.tar.gz"
+URL="https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/1.21.9-4905428782546944/linux-x64/Antigravity.tar.gz"
 TMP_FILE="/tmp/Antigravity.tar.gz"
 INSTALL_DIR="/opt/Antigravity"
 DESKTOP_DIR="/usr/share/applications"
